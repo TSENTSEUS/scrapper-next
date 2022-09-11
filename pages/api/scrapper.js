b053ca5b248cae8aca30905e788c526bfd7076e2
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 00:38:25 +0300

    testing

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -13,20 +13,20 @@ export default function handler(req,res) {
                 const $ = cheerio.load(res.data);
                 $('.title-info-title-text').each((i, e) => {
                     const test = $(e).text();
-                    test.push(obj)
+                    obj.push(test)
                 })
                 $('li[class=params-paramsList__item-appQw]').each((i, e) => {
                     const style = $(e).find('style').remove()
                     const text = $(e).text()
-                    text.push(obj)
+                    obj.push(text)
                 })
                 $('.style-price-value-main-TIg6u').each((i, e) => {
                     const price = $(e).find('span').attr('content')
-                    price.push(obj)
+                    obj.push(price)
                 })
                 $('.style-item-address__string-wt61A').each((i, e) => {
                     const location = $(e).text()
-                    location.push(obj)
+                    obj.push(location)
                 })
                 return console.log(obj)
             }).catch(err => console.log(err));
