2a0a1ed0fcb6f35e53d2a6051dc6587943402d1f
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 01:18:02 +0300

    testing

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ import {useEffect, useState} from "react";
 
 export default function Home() {
     const [url,setUrl] = useState('')
-    const [data, setData] = useState(null)
+    const [data, setData] = useState('')
     async function postRequest(e){
         e.preventDefault()
         const response = await axios.post('/api/scrapper', {url})
-        setData(response)
+        setData(response.data)
   }
 
 
