3a756ce72de54d1846be1d94ef3ffc0dcd0eb61d
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 13:18:51 +0300

    img parse

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -13,17 +13,16 @@ export default async function handler(req,res) {
                 const about = []
                 const obj = {}
                 const list = [obj]
-                list.push(obj['description'] = about)
-                list.push(obj['images'] = img)
+                obj['description'] = about
+                obj['images'] = img
                 const $ = cheerio.load(res.data);
                 $('.title-info-title-text').each((i, e) => {
                     const test = $(e).text();
-                    list.push(obj['title'] = test)
+                    obj['title'] = test
                 })
                 $('.images-preview-previewImageWrapper-RfThd img').each((i,e )=>{
                     const src = $(e).attr('src')
                     img.push(src)
-                    console.log(src)
                 })
                 $('li[class=params-paramsList__item-appQw]').each((i, e) => {
                     const style = $(e).find('style').remove()
@@ -32,11 +31,11 @@ export default async function handler(req,res) {
                 })
                 $('.style-price-value-main-TIg6u').each((i, e) => {
                     const price = $(e).text()
-                    list.push(obj['price'] = price)
+                  obj['price'] = price
                 })
                 $('.style-item-address__string-wt61A').each((i, e) => {
                     const location = $(e).text()
-                    list.push(obj['location'] = location)
+                   obj['location'] = location
                 })
                 console.log(list)
                 return list
