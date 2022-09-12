3aa0168ed0541e624462f0db4b358832db4c8a5d
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 12:25:39 +0300

    1

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -9,27 +9,28 @@ export default async function handler(req,res) {
     try {
         const result = await axios.get(req.body.url)
             .then(res => {
-                const obj = []
+                const obj = {}
+                const list = [obj]
                 const $ = cheerio.load(res.data);
                 $('.title-info-title-text').each((i, e) => {
                     const test = $(e).text();
-                    obj.push(test)
+                    list.push(obj['title'] = test)
                 })
                 $('li[class=params-paramsList__item-appQw]').each((i, e) => {
                     const style = $(e).find('style').remove()
                     const text = $(e).text()
-                    obj.push(text)
+                    list.push(obj['description'] = text)
                 })
                 $('.style-price-value-main-TIg6u').each((i, e) => {
                     const price = $(e).text()
-                    obj.push(price)
+                    list.push(obj['price'] = price)
                 })
                 $('.style-item-address__string-wt61A').each((i, e) => {
                     const location = $(e).text()
-                    obj.push(location)
+                    list.push(obj['location'] = location)
                 })
-                console.log(obj)
-                return obj
+                console.log(list)
+                return list
             }).catch(err => console.log(err));
         res.json(result)
     }catch (e){
