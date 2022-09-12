5c131209c971cfa84104eb11cf147e47494593d2
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 22:43:35 +0300

    custom input

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,17 @@ export default function Home() {
     const [url,setUrl] = useState('')
     const [data, setData] = useState([])
 
-    const [edit,setEdit] = useState(false)
-
     const [initialPrice, setInitialPrice] = useState('')
+    const [flatLocation, setFlatLocation] = useState('')
 
-    function editData(e){
-        e.preventDefault()
-        setEdit(!edit)
-    }
+    const [data2,setData2] = useState('Bebrikiva 32')
+
+
+
     useEffect(()=>{
         const price = data[0]?.price
+        const location = data[0]?.location
+        setFlatLocation(location)
         setInitialPrice(price)
     },[data])
 
@@ -46,14 +47,9 @@ export default function Home() {
         { data.length !== 0 ? data.map((type) => {
             return <>
                 <h3 > {type.title}</h3>
-                Стоимость {
-                    edit ?
-                    <input value={initialPrice} onChange={(e) =>
-                        setInitialPrice(e.target.value)}/> : initialPrice
-            }
-                <button onClick={editData}> Изменить цену </button>
+                <CustomInput value={initialPrice}/>
                 <div>
-                О квартире:
+                 О квартире:
                 <ul className={styles.description}>
                     {type.description.map((e,i) => <li key={i}>{e}</li>)}
                 </ul>
@@ -61,11 +57,33 @@ export default function Home() {
                 <div className={styles.gallery}>
                     {type.images.map((e,i) => <img src={e} alt={''} key={i} />)}
                 </div>
-                <div> <h4>Расположение:</h4> {type.location} </div>
+                <div> <h4>Расположение:</h4> <CustomInput value={flatLocation}/> </div>
             </>
         }) : ''}
 
-
+        <div>
+            <CustomInput value={'bebra'}/>
+            <CustomInput value={'bebra'}/>
+        </div>
     </div>
   )
+}
+
+const CustomInput = ({value}) => {
+    function editData(e){
+        e.preventDefault()
+        setEdit(!edit)
+    }
+    const [edit,setEdit] = useState(false)
+    const [data1, setData1] = useState(String(value))
+    return (
+        <div>
+            {edit
+            ? <input value={data1} onChange={(e) => setData1(e.target.value)}/>
+            : data1}
+            <button onClick={editData}>
+                edit
+            </button>
+        </div>
+    )
 }
