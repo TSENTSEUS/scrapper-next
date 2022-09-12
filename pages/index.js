81221d7b1138d800f213421e5a942ad9cf884f9a
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-12 13:44:39 +0300

    description

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
         { data.length !== 0 ? data.map((el,i) => {
             return <>
                 <h3 key={i}> {el.title}</h3>
-                <Image src={el.images[0]} alt={''}/>
+                {el.images.map((e,i) => <Image src={e} alt={''} key={i}/>)}
                 {el.description.map((e,i) => <p key={i}>{e}</p>)}
             </>
         }) : ''}
