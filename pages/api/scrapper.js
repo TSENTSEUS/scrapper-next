1467858532932619e84907b94947266df72de05b
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-13 18:44:25 +0300

    ADD | trying to upload all img in high quality

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -24,7 +24,7 @@ export default async function handler(req,res) {
                     const title = $(e).text();
                     obj['title'] = title
                 })
-                $('.images-preview-previewImageWrapper-RfThd img').each((i,e )=>{
+                $('.image-frame-wrapper-_NvbY img').each((i,e )=>{
                     const src = $(e).attr('src')
                     img.push(src)
                 })
