452e09abbc8f35540d778858c6fb4b82c9463b4c
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-13 13:00:32 +0300

    adding headers for beat cors policy

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -10,7 +10,7 @@ export default async function handler(req,res) {
         const result = await axios.get(req.body.url,{
            headers:{
                "Access-Control-Allow-Credentials": "true",
-               "Access-Control-Allow-Origin": "*" ,
+               "Access-Control-Allow-Origin": "https://scrapper-next.herokuapp.com" ,
            }
         })
             .then(res => {
