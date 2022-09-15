d06f386c5f8aef1340233fb05fed622a09114cf0
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-15 18:49:04 +0300

    FIX | cors fix

--- pages/api/finalData.js
+++ pages/api/finalData.js
@@ -1,12 +1,32 @@
-import CorsMiddleware from "../../middlewares/NextCors";
+import NextCors from 'nextjs-cors';
 const axios = require('axios')
-
+import Cors from 'cors'
 /**
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
+const cors = Cors({
+    methods: ['POST', 'GET', 'HEAD'],
+})
+export default async function handler(req, res) {
+    function runMiddleware(
+        req,
+        res,
+        fn,
+    ) {
+        return new Promise((resolve, reject) => {
+            fn(req, res, (result) => {
+                if (result instanceof Error) {
+                    return reject(result)
+                }
 
-const handler = async (req, res) => {
+                return resolve(result)
+            })
+        })
+    }
+
+    await runMiddleware(req, res, cors)
+
     const result = await axios({
         method:"post",
         url:"https://scrapper-next.herokuapp.com/api/scrapper",
@@ -22,5 +42,3 @@ const handler = async (req, res) => {
     console.log(result.data)
     res.send(result.data);
 }
-
-export default CorsMiddleware(handler)
