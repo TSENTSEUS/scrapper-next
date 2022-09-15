ffead10cfd5c543f0cc9f0bbd6c9654c2f7fdf10
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-15 13:25:22 +0300

    FIX | trying to defeat cors

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -7,11 +7,11 @@ const axios = require('axios')
 
 export default async function handler(req,res) {
 
-        const result = await axios.get(req.body.url,{
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "origin, x-requested-with, content-type",
-                "Access-Control-Allow-Methods": "PUT, GET, POST, DELETE, OPTIONS"
+        const result = await axios({
+            method:"get",
+            url:req.body.url,
+            headers:{
+                "Access-Control-Allow-Origin":"*"
             }
         })
             .then(res => {
