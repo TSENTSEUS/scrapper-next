362dc49fa4804a7cb4ae3277f8eb7d2cff14cf5d
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-15 17:29:17 +0300

    FIX | cors fix

--- pages/api/finalData.js
+++ pages/api/finalData.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     await NextCors(req, res, {
         // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        origin: 'https://scrapper-next.herokuapp.com/',
+        origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
     const result = await axios({
