393d7185958ad119538f90c02133da68fdf7b771
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-16 12:49:09 +0300

    FIX | CORS

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
+        destination: 'https://scrapper-next.herokuapp.com'
       }
     ]
   }
