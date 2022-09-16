25d48f62b4d778e5eb0005b9c9e930e9ab069691
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-09-16 15:55:07 +0300

    FIX | CORS

--- pages/api/scrapper.js
+++ pages/api/scrapper.js
@@ -23,7 +23,7 @@ function runMiddleware(req, res, fn) {
 async function handler(req,res) {
     await NextCors(req, res, {
         // Options
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE',"OPTIONS"],
         origin: 'https://scrapper-next.herokuapp.com',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
@@ -62,7 +62,7 @@ async function handler(req,res) {
                 console.log(list)
                 return list
             }).catch(err => console.log(err));
-
+         res.setHeader('GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE',"OPTIONS")
          res.json(result)
 }
 export default handler
